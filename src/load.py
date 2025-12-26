5f4b903607cf2e5b8b330bf82ae3bd26ff394b9e
utkarsha-a <utkarsha.aherrao@gmail.com>
2025-12-26 18:33:51 +0530

    adding the load layer, transfored data into sql db

---
+++ src/load.py
@@ -0,0 +1,9 @@
+from sqlalchemy import create_engine
+import pandas as pd
+
+def load_data(df: pd.DataFrame, table_name: str = "students") -> None:
+    engine = create_engine("sqlite:///students.db")
+    df.to_sql(table_name, engine, if_exists="replace", index=False)
+
+    print(f"[LOAD] Successfully loaded {len(df)} records" f"into table {table_name}")
+    
--- src/main.py
+++ src/main.py
@@ -1,7 +1,8 @@
 from extract import extract_data
+from load import load_data
 from transform import transform_data
 
 if __name__ == "__main__":
     df_raw = extract_data("data/raw/studentsRaw.csv")
     df_clean = transform_data(df_raw)
-    print(df_clean)
+    load_data(df_clean)
