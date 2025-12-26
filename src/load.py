58b21d9d07af873fb3bd91823a3547f0ce052135
utkarsha-a <utkarsha.aherrao@gmail.com>
2025-12-26 18:37:34 +0530

    adding the load layer, transformed data into sql db

--- src/load.py
+++ src/load.py
@@ -5,5 +5,5 @@ def load_data(df: pd.DataFrame, table_name: str = "students") -> None:
     engine = create_engine("sqlite:///students.db")
     df.to_sql(table_name, engine, if_exists="replace", index=False)
 
-    print(f"[LOAD] Successfully loaded {len(df)} records" f"into table {table_name}")
+    print(f"[LOAD] Successfully loaded {len(df)} records" f"into table {table_name}")  
     
